fde9affd1496b8d968a58423b1da403108c34a99
hhj <1291536750@qq.com>
2020-02-04 06:37:55 -0800

    lab03 part2

--- lab03/lfsr.c
+++ lab03/lfsr.c
@@ -5,6 +5,24 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    // YOUR CODE HERE 
+	uint16_t bit0 = (*reg)&1;
+	uint16_t bit2 = (*reg)>>2&1;
+	uint16_t bit3 = (*reg)>>3&1;
+	uint16_t bit5 = (*reg)>>5&1;
+	
+	uint16_t add = ((bit0^bit2)^bit3)^bit5;
+	add = add << 15;
+	uint16_t zero = 0;
+	zero = ~zero;
+	zero = zero >> 1;
+	add = add | zero;
+	
+	uint16_t one = 1;
+	one = one << 15;
+	*reg = (*reg) >> 1;
+	*reg = (*reg) | one; //change the left most to 1;
+
+	*reg = (*reg)&add;
 }
 
