c456450a6549656cce9d83f1ad1c355059ad2779
hhj <1291536750@qq.com>
2020-02-05 03:40:42 -0800

    lab03 part3

--- lab03/vector.c
+++ lab03/vector.c
@@ -26,7 +26,7 @@ vector_t *bad_vector_new() {
 
     /* Initialize attributes */
     retval->size = 1;
-    retval->data = malloc(sizeof(int));
+    retval->data = (int*)malloc(sizeof(int));
     if (retval->data == NULL) {
         allocation_failed();
     }
@@ -42,7 +42,7 @@ vector_t also_bad_vector_new() {
 
     /* Initialize attributes */
     v.size = 1;
-    v.data = malloc(sizeof(int));
+    v.data = (int*)malloc(sizeof(int));
     if (v.data == NULL) {
         allocation_failed();
     }
@@ -58,27 +58,27 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = (vector_t*)malloc(sizeof(vector_t));
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval == NULL) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 1;
+    retval->data = (int*)malloc(sizeof(int)*(retval->size));
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval->data == NULL) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    retval->data[0] = 0;
 
     /* and return... */
     return retval;
@@ -96,8 +96,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size) {
+        return v->data[loc];
     } else {
         return 0;
     }
@@ -106,7 +106,8 @@ int vector_get(vector_t *v, size_t loc) {
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
-    /* YOUR SOLUTION HERE */
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -115,6 +116,33 @@ void vector_set(vector_t *v, size_t loc, int value) {
     /* What do you need to do if the location is greater than the size we have
      * allocated?  Remember that unset locations should contain a value of 0.
      */
+    // magic; make me deep in the struct and the pointer;
+    /* //the answer is true. but the memory check is wrong.
+    if(loc < v->size){
+        v->data[loc] = value;
+    } else { // auto grow up
+        size_t len = v->size;
+        v->size = loc+1;
+        for(int i=len; i<loc; i++){
+            v->data[i] = 0;
+        }
+        v->data[loc] = value;
+    }*/
+    if(loc < v->size){
+        v->data[loc] = value;
+    } else { // auto grow up
+        int *res = (int*)malloc(sizeof(int)*(loc+1));
+        for(int i=0; i<v->size; i++){
+            res[i] = v->data[i]; 
+        }
+        for(int i=v->size; i<(loc+1); i++){
+            res[i] = 0;
+        }
+        free(v->data);
+        v->size = loc+1;
+        v->data = res;
+        v->data[loc] = value;
+    }
+    
 
-    /* YOUR SOLUTION HERE */
 }
