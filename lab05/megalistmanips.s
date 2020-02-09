874530b5abf5280913cfc1c75e659cba6e263b9b
hao <1291536750@qq.com>
2020-02-09 21:15:22 +0800

    lab05 part1 要理解risc-v内存的接口

--- lab05/megalistmanips.s
+++ lab05/megalistmanips.s
@@ -62,22 +62,24 @@ map:
     # - 4 for the size of the array
     # - 4 more for the pointer to the next node
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
+    lw t4, 0(s0)        # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
-    lw a0, 0(t1)        # load the value at that address into a0
+    slli t3, t0, 2
+    add t4, t4, t3      # offset the array address by the count
+    lw a0, 0(t4)        # load the value at that address into a0
 
     jalr s1             # call the function on that value.
 
-    sw a0, 0(t1)        # store the returned value back into the array
+    sw a0, 0(t4)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    #a1 保存function的地址不需要改变
+    #lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
 
-    jal  map            # recurse
+    jal ra, map            # recurse
 done:
     lw s0, 8(sp)
     lw s1, 4(sp)
