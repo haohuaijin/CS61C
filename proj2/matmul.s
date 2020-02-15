b4de72e06a6bc19585c3761463dad304851e7d25
hao <1291536750@qq.com>
2020-02-15 22:39:08 +0800

    finish proj2 matmul,s

--- proj2/matmul.s
+++ proj2/matmul.s
@@ -34,19 +34,16 @@ matmul:
     add s0 a0 x0 # the pointer of m0
     add s1 a3 x0 # the pointer of m1
     add s2 a6 x0 # the pointer of the result
-    add s3 a2 x0 # the length of the vector
     add s4 a1 x0 # the rows of the m0
     add s5 a5 x0 # the colouns of the m1
 
     addi a3 x0 1 # the stride of m0 vector
     add a4 s5 x0 # the stride of m1 vector
-    add a2 s3 x0 # the length of vector
 
     add a5 s0 x0 # use m0 pointer in loop
     add a6 s1 x0 # use m1 pointer in loop
 
-    add t4 x0 x0 # use to end the out loop i
-
+    add s3 x0 x0 # use to end the out loop i
 
 outer_loop_start:
     add t5 x0 x0 # use to the innner loop j
@@ -54,13 +51,14 @@ outer_loop_start:
     add a0 a5 x0 # m0
     add a1 a6 x0 # m1
 
-
 inner_loop_start:
 
+    addi a3 x0 1 # the stride of m0 vector
+    add a4 s5 x0 # the stride of m1 vector
     jal ra dot
 
     # find the index of result
-    mul t6 t4 s3
+    mul t6 s3 a2
     add t6 t6 t5
     slli t6 t6 2
     add t6 t6 s2
@@ -69,33 +67,35 @@ inner_loop_start:
 
     # modified the address
     addi t5 t5 1
+
     add a0 a5 x0
     slli t6 t5 2
     add a1 a6 t6
 
-    beq t5 a2 inner_loop_end
+    beq t5 s5 inner_loop_end
     j inner_loop_start
 
 inner_loop_end:
 
-    slli t6 s2 2 
+    slli t6 a2 2 
     add a5 a5 t6
 
-    addi t4 t4 1
-    beq t4 s4 outer_loop_end
+    addi s3 s3 1
+    beq s3 s4 outer_loop_end
     j outer_loop_start
 
 outer_loop_end:
 
+    mv a6 s2
 
     # Epilogue
-    sw s5 24(sp)
-    sw s4 20(sp)
-    sw s3 16(sp)
-    sw s2 12(sp)
-    sw s1 8(sp) 
-    sw s0 4(sp)
-    sw ra 0(sp) 
+    lw s5 24(sp)
+    lw s4 20(sp)
+    lw s3 16(sp)
+    lw s2 12(sp)
+    lw s1 8(sp) 
+    lw s0 4(sp)
+    lw ra 0(sp) 
     addi sp sp 28
     ret
 
