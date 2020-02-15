e23d2246c1a523b9b4e65c026b4f6c192867ce28
hao <1291536750@qq.com>
2020-02-15 22:05:33 +0800

    ...

--- proj2/test_files/test_matmul.s
+++ proj2/test_files/test_matmul.s
@@ -11,19 +11,22 @@ d: .word 0 0 0 0 0 0 0 0 0 # allocate static space for output
 .text
 main:
     # Load addresses of input matrices (which are in static memory), and set their dimensions
-	    
-
-
+	la a0 m0
+    addi a1 x0 3
+    addi a2 x0 3
+    la a3 m1
+    addi a4 x0 3
+    addi a5 x0 3
+    la a6 d
 
     # Call matrix multiply, m0 * m1
-    
-
-
+    jal ra matmul
 
     # Print the output (use print_int_array in utils.s)
-
-
-
+    mv a0 a6
+    addi a1 x0 3
+    addi a2 x0 3
+    jal ra print_int_array
 
     # Exit the program
     jal exit
