b561a42278cbf92b25afb3cf8978c1a892029e55
hao <1291536750@qq.com>
2020-02-16 22:03:29 +0800

    proj2 read_matrix.s first

--- proj2/read_matrix.s
+++ proj2/read_matrix.s
@@ -19,18 +19,69 @@
 read_matrix:
 
     # Prologue
-	
+	addi sp sp -16
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
+    sw s2 12(sp)
+    
+    mv s0 a0
+    mv s1 a1
+    mv s2 a2
 
+    # open file
+    addi a1 s0 0
+    addi a2 x0 0
+    jal ra fopen
+    #当打开文件失败时
+    addi t0 x0 -1 
+    beq a0 t0 eof_or_error
 
+    mv s0 a0 # save the file descriptor
 
+    # read the rows
+    mv a1 s0
+    mv a2 s1
+    addi a3 x0 4
+    jal ra fread 
+    addi t0 x0 4
+    bne a0 t0 eof_or_error
+    lw t1 0(a2) # save rows to t1
 
+    # read coloums
+    mv a1 s0
+    mv a2 s2
+    addi a3 x0 4
+    jal ra fread 
+    addi t0 x0 4
+    bne a0 t0 eof_or_error
+    lw t2 0(a2) # save coloums to t2
 
+    # read matrix
+    mul a0 t1 t2
+    slli a0 a0 2
+    jal ra malloc
+    beq a0 x0 eof_or_error # 如果未分配，报错
 
+    mv a1 s0
+    mv a2 a0
+    mul a3 t1 t2
+    slli a3 a3 2
+    jal ra fread
+    mul t0 t1 t2
+    slli t0 t0 2
+    bne t0 a0 eof_or_error
 
+    mv a0 a2 
+    mv a1 s1
+    mv a2 s2
 
     # Epilogue
-
-
+    lw s2 12(sp)
+    lw s1 8(sp)
+    lw s0 4(sp)
+    lw ra 0(sp)
+    addi sp sp 4
     ret
 
 eof_or_error:
--- proj2/test_files/test_read_matrix.s
+++ proj2/test_files/test_read_matrix.s
@@ -7,10 +7,22 @@ file_path: .asciiz "./test_input.bin"
 .text
 main:
     # Read matrix into memory
+    addi a0 x0 4
+    jal ra malloc
+    mv a1 a0
 
+    addi a0 x0 4
+    jal ra malloc
+    mv a2 a0
+
+    la a0 file_path
+
+    jal ra read_matrix
 
     # Print out elements of matrix
-
+    lw a1 0(a1)
+    lw a2 0(a2)
+    jal ra print_int_array
 
     # Terminate the program
     addi a0, x0, 10
