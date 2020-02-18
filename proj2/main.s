e5a144da78cf70d7b9564c0fbe74877f233ccd7b
hao <1291536750@qq.com>
2020-02-18 08:03:29 +0800

    ...

--- proj2/main.s
+++ proj2/main.s
@@ -32,7 +32,7 @@ main:
 
 # Load pretrained m0
     
-    lw s1 8(s0)
+    lw s1 4(s0)
     #malloc the rows
     addi a0 x0 4
     jal ra malloc
@@ -41,7 +41,7 @@ main:
     addi a0 x0 4
     jal ra malloc
     mv a2 a0
-    
+
     mv a0 s1 
     jal ra read_matrix
     
@@ -50,130 +50,130 @@ main:
     lw s3 0(a2) # the columns
 
 
-# # Load pretrained m1
+# Load pretrained m1
 
-#     lw s4 8(s0)
-#     #malloc the rows
-#     addi a0 x0 4
-#     jal ra malloc
-#     mv a1 a0
-#     #malloc the columns
-#     addi a0 x0 4
-#     jal ra malloc
-#     mv a2 a0
+    lw s4 8(s0)
+    #malloc the rows
+    addi a0 x0 4
+    jal ra malloc
+    mv a1 a0
+    #malloc the columns
+    addi a0 x0 4
+    jal ra malloc
+    mv a2 a0
 
-#     mv a0 s4 
-#     jal ra read_matrix
+    mv a0 s4 
+    jal ra read_matrix
     
-#     mv s4 a0 # the pointer
-#     lw s5 0(a1) # the row
-#     lw s6 0(a2) # the columns
+    mv s4 a0 # the pointer
+    lw s5 0(a1) # the row
+    lw s6 0(a2) # the columns
 
 
-# # Load input matrix
+# Load input matrix
 
-#     lw s7 12(s0)
-#     #malloc the rows
-#     addi a0 x0 4
-#     jal ra malloc
-#     mv a1 a0
-#     #malloc the columns
-#     addi a0 x0 4
-#     jal ra malloc
-#     mv a2 a0
+    lw s7 12(s0)
+    #malloc the rows
+    addi a0 x0 4
+    jal ra malloc
+    mv a1 a0
+    #malloc the columns
+    addi a0 x0 4
+    jal ra malloc
+    mv a2 a0
 
-#     mv a0 s7 
-#     jal ra read_matrix
+    mv a0 s7 
+    jal ra read_matrix
     
-#     mv s7 a0 # the pointer
-#     lw s8 0(a1) # the row
-#     lw s9 0(a2) # the columns
+    mv s7 a0 # the pointer
+    lw s8 0(a1) # the row
+    lw s9 0(a2) # the columns
     
 
 
-#     # =====================================
-#     # RUN LAYERS
-#     # =====================================
-#     # 1. LINEAR LAYER:    m0 * input
-#     # 2. NONLINEAR LAYER: ReLU(m0 * input)
-#     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
+    # =====================================
+    # RUN LAYERS
+    # =====================================
+    # 1. LINEAR LAYER:    m0 * input
+    # 2. NONLINEAR LAYER: ReLU(m0 * input)
+    # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
 
-# # first matmul
-#     mul a0 s2 s9 
-#     slli a0 a0 2
-#     jal ra malloc
+# first matmul
+    mul a0 s2 s9 
+    slli a0 a0 2
+    jal ra malloc
 
-#     mv a6 a0 # the result
-#     mv a0 s1
-#     mv a1 s2
-#     mv a2 s3
-#     mv a3 s7
-#     mv a4 s8
-#     mv a5 s9
-#     jal ra matmul
+    mv a6 a0 # the result
+    mv a0 s1
+    mv a1 s2
+    mv a2 s3
+    mv a3 s7
+    mv a4 s8
+    mv a5 s9
+    jal ra matmul
 
-#     # save the result
-#     mv s1 a6
-#     mv s2 a1 # rows
-#     mv s3 a5 # columns
-
-
-# # second relu
-#     mv a0 s1
-#     mul a1 s2 s3
-#     jal ra relu
+    # save the result
+    mv s1 a6
+    mv s2 a1 # rows
+    mv s3 a5 # columns
 
 
-# # third matmul
-#     mul a0 s5 s3
-#     slli a0 a0 2
-#     jal ra malloc
-
-#     mv a6 a0
-#     mv a0 s4
-#     mv a1 s5
-#     mv a2 s6
-#     mv a3 s1
-#     mv a4 s2
-#     mv a5 s3
-#     jal ra matmul
-
-#     # save the result
-#     mv s1 a6
-#     mv s2 a1 # rows
-#     mv s3 a5 # columns
+# second relu
+    mv a0 s1
+    mul a1 s2 s3
+    jal ra relu
 
 
+# third matmul
+    mul a0 s5 s3
+    slli a0 a0 2
+    jal ra malloc
 
-#     # =====================================
-#     # WRITE OUTPUT
-#     # =====================================
-#     # Write output matrix
-#     lw a0 16(s0) # Load pointer to output filename
-#     mv a1 s1 # pointer
-#     mv a2 s2 # rows
-#     mv a3 s3 # columns
-#     jal ra write_matrix
+    mv a6 a0
+    mv a0 s4
+    mv a1 s5
+    mv a2 s6
+    mv a3 s1
+    mv a4 s2
+    mv a5 s3
+    jal ra matmul
+
+    # save the result
+    mv s1 a6
+    mv s2 a1 # rows
+    mv s3 a5 # columns
 
 
 
-#     # =====================================
-#     # CALCULATE CLASSIFICATION/LABEL
-#     # =====================================
-#     # Call argmax
-#     mv a0 s1
-#     mul a1 s2 s3
-#     jal ra argmax
+    # =====================================
+    # WRITE OUTPUT
+    # =====================================
+    # Write output matrix
+    lw a0 16(s0) # Load pointer to output filename
+    mv a1 s1 # pointer
+    mv a2 s2 # rows
+    mv a3 s3 # columns
+    jal ra write_matrix
 
 
-#     # Print classification
-#     mv a1 a0
-#     jal ra print_int
+
+    # =====================================
+    # CALCULATE CLASSIFICATION/LABEL
+    # =====================================
+    # Call argmax
+    mv a0 s1
+    mul a1 s2 s3
+    jal ra argmax
 
 
-#     # Print newline afterwards for clarity
-#     li a1 '\n'
-#     jal print_char
+    # Print classification
+    mv a1 a0
+    jal ra print_int
+
+
+    # Print newline afterwards for clarity
+    li a1 '\n'
+    jal print_char
 
     jal exit
 
