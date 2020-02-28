8e57300961da0f5281430404bb3025da93c36190
hhj <1291536750@qq.com>
2020-02-27 23:05:14 -0800

    finish proj3

---
+++ proj3/testing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./cpu-part1-pipelined.sh
+cd part1-tests/addi-tests
+python3 binary_to_hex.py circ_files/student_output/CPU-addi-pipelined.out
+python3 binary_to_hex.py circ_files/reference_output/CPU-addi-pipelined.out
+cd ../..
+java -jar logisim-evolution.jar
