c591ad3249bd2a2f110519cbcae0209d5091df31
hhj <1291536750@qq.com>
2020-03-04 05:25:44 -0800

    proj3.2 test R....

--- proj3/test-two.sh
+++ proj3/test-two.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ./cpu-twostage-sanity.sh
-python3 binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-addi.out
 printf "\n"
-python3 binary_to_hex.py twostage-tests/circ_files/output/CPU-addi.out
+python3 binary_to_hex.py twostage-tests/circ_files/output/CPU-add_lui_sll.out
+printf "\n"
+python3 binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-add_lui_sll.out
 java -jar logisim-evolution.jar
