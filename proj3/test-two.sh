18fc75d55629174e9f1336a5e086bba9ac9f503d
hao <1291536750@qq.com>
2020-03-05 20:39:09 +0800

    proj3.2 finish part 1 and 2

--- proj3/test-two.sh
+++ proj3/test-two.sh
@@ -2,7 +2,7 @@
 
 ./cpu-twostage-sanity.sh
 printf "\n"
-python3 binary_to_hex.py twostage-tests/circ_files/output/CPU-add_lui_sll.out
+python binary_to_hex.py twostage-tests/circ_files/output/CPU-add_lui_sll.out
 printf "\n"
-python3 binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-add_lui_sll.out
+python binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-add_lui_sll.out
 java -jar logisim-evolution.jar
--- proj3/testing.sh
+++ proj3/testing.sh
@@ -2,8 +2,8 @@
 
 ./cpu-part1-pipelined.sh
 cd part1-tests/addi-tests
-python3 binary_to_hex.py circ_files/student_output/CPU-addi-pipelined.out
+python binary_to_hex.py circ_files/student_output/CPU-addi-pipelined.out
 printf "\n"
-python3 binary_to_hex.py circ_files/reference_output/CPU-addi-pipelined.out
+python binary_to_hex.py circ_files/reference_output/CPU-addi-pipelined.out
 cd ../..
 java -jar logisim-evolution.jar
