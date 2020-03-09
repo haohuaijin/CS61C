3b42cffcf8a586f5ed51197f00bacc985314c8de
hao <1291536750@qq.com>
2020-03-09 21:06:50 +0800

    proj3 finish branch test

--- proj3/test-two.sh
+++ proj3/test-two.sh
@@ -2,7 +2,7 @@
 
 ./cpu-twostage-sanity.sh
 printf "\n"
-python binary_to_hex.py twostage-tests/circ_files/output/CPU-branches.out
+python binary_to_hex.py twostage-tests/circ_files/output/CPU-jump.out
 printf "\n"
-python binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-branches.out
+python binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-jump.out
 java -jar logisim-evolution.jar
