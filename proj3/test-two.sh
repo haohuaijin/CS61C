32b5c53e7786125c61960d5369ee67b9f79d2719
hao <1291536750@qq.com>
2020-03-09 15:55:14 +0800

    proj3 finish CPU memory test

--- proj3/test-two.sh
+++ proj3/test-two.sh
@@ -2,7 +2,7 @@
 
 ./cpu-twostage-sanity.sh
 printf "\n"
-python binary_to_hex.py twostage-tests/circ_files/output/CPU-add_lui_sll.out
+python binary_to_hex.py twostage-tests/circ_files/output/CPU-branches.out
 printf "\n"
-python binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-add_lui_sll.out
+python binary_to_hex.py twostage-tests/circ_files/reference_output/CPU-branches.out
 java -jar logisim-evolution.jar
